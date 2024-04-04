6be5510cf079b3330c4b2764aadd41a52afb8c3c
Luiz Bello <luizpaulopinto25@gmail.com>
2024-04-04 19:08:38 -0300

    chore(schedule): adicionando tabela pivot ao import do modulo

--- src/client/schedules/schedules.module.ts
+++ src/client/schedules/schedules.module.ts
@@ -3,9 +3,10 @@ import { SchedulesService } from './schedules.service';
 import { SchedulesController } from './schedules.controller';
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { Schedule } from "../../database/src/typeorm/entities/schedules.entity";
+import { ScheduledPets } from "../../database/src/typeorm/entities/scheduled-pets.entity";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Schedule])],
+  imports: [TypeOrmModule.forFeature([Schedule, ScheduledPets])],
   controllers: [SchedulesController],
   providers: [SchedulesService],
 })
